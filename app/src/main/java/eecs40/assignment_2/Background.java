3a8387f9e3c1ef593dd64ad777c09c0caf689171
Michael Herrera <michaerh@uci.edu>
2015-04-22 18:20:45 -0700

    Fixed scrolling lag

--- app/src/main/java/eecs40/assignment_2/Background.java
+++ app/src/main/java/eecs40/assignment_2/Background.java
@@ -10,6 +10,7 @@ import android.graphics.Rect;
 public class Background implements TimeConscious {
 
     private final DashTillPuffSurfaceView   view;
+    Bitmap                                  bitmap1, bitmap2;
     public  int                             x1, X1;
     public  int                             x2, X2;
     public  int                             y1, Y1;
@@ -18,6 +19,9 @@ public class Background implements TimeConscious {
 
     public Background ( DashTillPuffSurfaceView view ) {
         this.view = view;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        this.bitmap1 = BitmapFactory.decodeResource(view.getResources(), R.drawable.dashtillpuffwallpaper, options);
+        this.bitmap2 = BitmapFactory.decodeResource(view.getResources(), R.drawable.dashtillpuffwallpaper, options);
         this.width = view.getWidth();
         this.height = view.getHeight();
         this.x1 = 0;
@@ -28,14 +32,13 @@ public class Background implements TimeConscious {
         this.X2 = this.X1 + width;
         this.Y1 = 0;
         this.Y2 = height;
-
     }
 
     public void tick( Canvas canvas ) {
-        this.x1 -= 20;
-        this.x2 -= 20;
-        this.X1 -= 20;
-        this.X2 -= 20;
+        this.x1 -= 10;
+        this.x2 -= 10;
+        this.X1 -= 10;
+        this.X2 -= 10;
         if (this.x2 <= 0) {
             this.x1 = this.X2;
             this.x2 = this.x1 + width;
@@ -48,12 +51,8 @@ public class Background implements TimeConscious {
     }
 
     protected void draw ( Canvas c ) {
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        Bitmap bitmap1 = BitmapFactory.decodeResource(view.getResources(), R.drawable.dashtillpuffwallpaper, options);
-        Bitmap bitmap2 = BitmapFactory.decodeResource(view.getResources(), R.drawable.dashtillpuffwallpaper, options);
 
         Paint paint = new Paint();
-        paint.setAlpha(255); // Control transparency
         Rect dst1 = new Rect( x1, y1, x2, y2 ); // Where to draw .
         Rect dst2 = new Rect( X1, Y1, X2, Y2 );
         c.drawBitmap ( bitmap1, null, dst1, paint );
